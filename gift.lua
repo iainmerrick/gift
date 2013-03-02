317d07e5bb7fe9e51676005673ebee6e7067ec4c
Iain Merrick <iain.merrick@gmail.com>
2013-03-02 11:39:17 +0000

    read32, read16, read8
    I expect most reads to be aligned, and reading 32-bit words from
    main memory is much more efficient than reading bytes. We therefore
    store Glulx memory as an array of int32s, and use bitwise masking
    and shifting to read smaller or unaligned values. LuaJIT's tracing
    JIT should inline this and strip out the checks in some cases.
    
    Thinking about the best way to store the global interpreter state.
    I did a few tiny benchmarks, and it looks like manually passing in
    a 'this' pointer is a decent strategy. For now, use a variable-size
    struct with Glulx memory at the end, so there's no extra pointer
    indirection; but we might want to store the stack there instead.

--- gift.lua
+++ gift.lua
@@ -1,6 +1,11 @@
 #!/usr/bin/env luajit
 
 bit = require("bit")
+band = bit.band
+bor = bit.bor
+lshift = bit.lshift
+rshift = bit.rshift
+
 ffi = require("ffi")
 io = require("io")
 string = require("string")
@@ -10,20 +15,73 @@ local size = file:seek("end")
 file:seek("set", 0)
 print("Size is:", size)
 
-assert(bit.band(size, 3) == 0, "Size must be a multiple of 4!")
+assert(band(size, 3) == 0, "Size must be a multiple of 4!")
 size = size / 4
 
-memory = ffi.new("uint32_t[?]", size)
+ffi.cdef[[
+typedef struct {
+  uint32_t memory[?];
+} GlulxState;
+]]
+GlulxState = ffi.typeof("GlulxState")
+state = ffi.new(GlulxState, size)
+
 for i = 0,size-1 do
   bytes = file:read(4)
-  memory[i] = bit.bor(
-    bit.lshift(bytes:byte(1), 24),
-    bit.lshift(bytes:byte(2), 16),
-    bit.lshift(bytes:byte(3), 8),
+  state.memory[i] = bor(
+    lshift(bytes:byte(1), 24),
+    lshift(bytes:byte(2), 16),
+    lshift(bytes:byte(3), 8),
     bytes:byte(4))
 end
 file.close()
 
-for i = 0,20 do
-  print(memory[i])
+function read32(g, addr)
+  local word = g.memory[rshift(addr, 2)]
+  local byte = band(addr, 3)
+  if byte == 0 then
+    return word
+  end
+  return bor(
+    lshift(read16(g, addr), 16),
+    read16(g, addr + 2))
+end
+
+function read16(g, addr)
+  local word = g.memory[rshift(addr, 2)]
+  local byte = band(addr, 3)
+  if byte == 0 then
+    return rshift(word, 16)
+  elseif byte == 2 then
+    return band(word, 0xffff)
+  end
+  return bor(
+    lshift(read8(g, addr), 8),
+    read8(g, addr + 1))
+end
+
+function read8(g, addr)
+  local word = g.memory[rshift(addr, 2)]
+  local byte = band(addr, 3)
+  if byte == 0 then
+    return rshift(word, 24)
+  elseif byte == 1 then
+    return band(rshift(word, 16), 0xff)
+  elseif byte == 2 then
+    return band(rshift(word, 8), 0xff)
+  else
+    return band(word, 0xff)
+  end
+end
+
+for i = 0,10 do
+  print(string.format("%08x", state.memory[i]))
+end
+
+for i = 0,7 do
+  print(string.format("%4d: %08x %04x %02x",
+    i,
+    read32(state, i),
+    read16(state, i),
+    read8(state, i)))
 end
